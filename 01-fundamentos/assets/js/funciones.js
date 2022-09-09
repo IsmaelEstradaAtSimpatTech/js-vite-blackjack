b8a39bafdd7e8a6ea827cab127e6ae981acf7ea8
Ismael Estrada <ismael.estrada@simpat.tech>
2022-09-09 16:11:00 -0500

    video 27 - funciones 2

--- 01-fundamentos/assets/js/funciones.js
+++ 01-fundamentos/assets/js/funciones.js
@@ -1,11 +1,13 @@
 function saludar(nombre) {
     console.log( arguments );
     console.log('Hola ' + nombre);
-}
+    return 1;
+} 
 
 
 const saludar2 = function() {
     console.log('Hola Mundo');
+    return [1,2,3,4];
 }
 
 const saludarFlecha = () => {
@@ -15,8 +17,30 @@ const saludarFlecha2 = ( nombre ) => {
     console.log('Hola ' + nombre );    
 }
 
-saludar('Pedro', 40, true, 'Saltillo');saludar('Lauro');saludar('Nadia');saludar();
+saludar('Pedro', 40, true, 'Saltillo');saludar('Lauro');saludar('Nadia');
+let retornoDeSaludar = saludar();
+console.log({ retornoDeSaludar });
+
 saludar2();saludar2();saludar2();saludar2();
+retornoDeSaludar = saludar2();
+console.log({ retornoDeSaludar });
 
 saludarFlecha();
 saludarFlecha2('Joaquin');
+
+function sumar(a, b) {
+    return a + b ;
+}
+
+console.log(sumar(1, 2));
+
+const sumar2 = (a, b) => {
+    return a + b ;
+}
+
+console.log(sumar2(1, 2));
+
+// funcion Lambda simplificada
+const getAleatorio = () => Math.random();
+
+console.log(getAleatorio());
