13a94fa286a60f3d99bb9c52198ab964bcc0eb7a
Ismael Estrada <ismael.estrada@simpat.tech>
2022-09-09 11:05:38 -0500

    video 21 - palabras reservadas

---
+++ 01-fundamentos/assets/js/palabras-reservadas.js
@@ -0,0 +1,15 @@
+
+
+// Las palabras reservadas no pueden usarse para nombre de identificadores
+// se recomiendo 
+// - camelCase para identificadores de variables
+// - UpperCamelCase para identificadores de Tipos y Clases
+// - UPPERCASE para constantes.
+
+// ecmascript 6 acepta muchos caracteres especiales dentro de los nombres de identificadores
+// pero es una mala practica
+
+// el underscore (_) es recomendado en lugar de signos de puntuacion dentro de 
+// nombres de identificadores.
+
+// el signo de dolar es ocasionalmente usado al inicio de los nombres de variables.
