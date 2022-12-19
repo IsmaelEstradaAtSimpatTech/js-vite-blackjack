c6301df2e1671eea020fbe06501d17efa7e15b2f
Ismael Estrada <ismael.estrada@simpat.tech>
2022-12-19 15:50:04 -0600

    video 73

--- 01-fundamentos/assets/js/classes/class.js
+++ 01-fundamentos/assets/js/classes/class.js
@@ -10,9 +10,20 @@ class Person {
         this.phrase = phrase;
     }
 
+    whoami() {
+        console.log(`I am ${this.name} and my secret identity is ${this.code} `);
+    }
+
+    myPhrase() {
+        console.log(`I say: ${this.phrase}`);
+    }
+
 }
 
 const spiderman = new Person('Peter Parker', 'Spiderman', 'I am your friendly neoghbor spiderman');;
 const ironman = new Person('Tony Stark', 'Ironman', 'I am ironman');
 console.log( {spiderman} );
 console.log( {ironman} );
+
+spiderman.whoami(); spiderman.myPhrase(); 
+ironman.whoami(); ironman.myPhrase();
